13fe7c3e6f91e933baa27f7f1d8239a74e844f2a
Mahfuzur | WebAble <mahfuzur.rahman@webable.com.bd>
2020-11-20 01:30:46 +0600

    turned off email verification during regidtration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -80,6 +81,7 @@ class RegisterController extends Controller
             'institution' => $data['institution'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'email_verified_at' => Carbon::now()
         ]);
     }
 }
