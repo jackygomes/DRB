76a090a9c9eadd77ee4d9c3e74683dfbaff79163
Mahfuzur | WebAble <mahfuzur.rahman@webable.com.bd>
2020-12-03 20:27:28 +0600

    resized single page news image

--- resources/views/front-end/news/single-news.blade.php
+++ resources/views/front-end/news/single-news.blade.php
@@ -61,7 +61,7 @@
                 <a href="{{route('news.index')}}" class="btn btn-outline-warning mb-3">All News</a>
                 <div class="bg-light shadow-sm border-bottom border-warning mb-5">
                     <div class="row" id="{{$news->id}}">
-                        <div class="col-md-5">
+                        <div class="col-md-3">
                             @if($news->image)
                                 <a href="{{$news->source}}" target="_blank">
                                     <img src="{{ env('S3_URL') }}{{$news->image}}" class="mr-3 img-fluid news-index-img" alt="...">
@@ -76,8 +76,8 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div  class="col-md-6 mt-2">
-                            <button type="button" class="btn btn-light btn-sm mb-2 mr-2 border border-secondary" @click='isshowcomment({{$news->id}})'><i class="far fa-comment-alt"></i> Comment</button>
+                        <div  class="col-md-6 mt-2 mb-2">
+                            {{--<button type="button" class="btn btn-light btn-sm mb-2 mr-2 border border-secondary" @click='isshowcomment({{$news->id}})'><i class="far fa-comment-alt"></i> Comment</button>--}}
 
                             <span>
                                 <div class="fb-share-button" data-href="{{route('news.single', $news->id)}}" data-layout="button_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u={{route('news.single', $news->id)}}&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
@@ -101,54 +101,54 @@
                                 </div>
                             </form>
                         @endif    
-                        <ul class="list-group mb-3">
-                            @foreach ($news->comments as $comment)
-                                <li class="list-group-item rounded small border-0 mb-1">
-                                    <div class="row">
-                                        <div class="col-md-12">
-                                            <b>{{$comment->user_id != null ? $comment->user->full_name : 'Anonymous'}}:</b> 
-                                            <span v-if="isShowCommentBox == {{$comment->id}}">
-                                                <form method="POST" action="{{ route('comment.update', $comment->id) }}">
-                                                    @csrf
-                                                    @method('patch')
-                                                    <div class="row">
-                                                        <div class="col-10">
-                                                            <div class="form-group">
-                                                                <textarea class="form-control mr-5" id="exampleFormControlTextarea1" name="body" rows="1" placeholder="Write a comment...">{{$comment->body}}</textarea>
-                                                            </div>
-                                                        </div>
-                                                        <div class="col-2">
-                                                            <input type="hidden" name="news_id" value="{{$news->id}}">
-                                                            <button type="submit" class="btn btn-warning w-100 float-right">Update</button>
-                                                        </div>
-                                                    </div>
-                                                </form>
-                                            </span> 
-                                            <span v-else>{{$comment->body}}</span>
-                                        </div>
+                        {{--<ul class="list-group mb-3">--}}
+                            {{--@foreach ($news->comments as $comment)--}}
+                                {{--<li class="list-group-item rounded small border-0 mb-1">--}}
+                                    {{--<div class="row">--}}
+                                        {{--<div class="col-md-12">--}}
+                                            {{--<b>{{$comment->user_id != null ? $comment->user->full_name : 'Anonymous'}}:</b> --}}
+                                            {{--<span v-if="isShowCommentBox == {{$comment->id}}">--}}
+                                                {{--<form method="POST" action="{{ route('comment.update', $comment->id) }}">--}}
+                                                    {{--@csrf--}}
+                                                    {{--@method('patch')--}}
+                                                    {{--<div class="row">--}}
+                                                        {{--<div class="col-10">--}}
+                                                            {{--<div class="form-group">--}}
+                                                                {{--<textarea class="form-control mr-5" id="exampleFormControlTextarea1" name="body" rows="1" placeholder="Write a comment...">{{$comment->body}}</textarea>--}}
+                                                            {{--</div>--}}
+                                                        {{--</div>--}}
+                                                        {{--<div class="col-2">--}}
+                                                            {{--<input type="hidden" name="news_id" value="{{$news->id}}">--}}
+                                                            {{--<button type="submit" class="btn btn-warning w-100 float-right">Update</button>--}}
+                                                        {{--</div>--}}
+                                                    {{--</div>--}}
+                                                {{--</form>--}}
+                                            {{--</span> --}}
+                                            {{--<span v-else>{{$comment->body}}</span>--}}
+                                        {{--</div>--}}
 
-                                        <div class="col-md-6">
-                                            <span class="text-secondary news-comment-time-text mt-n3">{{$comment->updated_at->diffForHumans()}}</span>
-                                        </div>
+                                        {{--<div class="col-md-6">--}}
+                                            {{--<span class="text-secondary news-comment-time-text mt-n3">{{$comment->updated_at->diffForHumans()}}</span>--}}
+                                        {{--</div>--}}
 
-                                        <div class="col-md-6 text-right">
-                                            @if(Auth::user())
-                                                @if(Auth::user()->id == $comment->user_id) 
-                                                    <button class="bg-transparent border-0 small text-secondary" @click="isComment({{$comment->id}})"  v-if="isShowCommentBox != {{$comment->id}}">Edit</button>
-                                                    <button class="bg-transparent border-0 small text-secondary" @click="isComment(null)"  v-if="isShowCommentBox == {{$comment->id}}">Cancel</button>
-                                                    <form action="{{ route('comment.destroy', $comment->id)}}" onclick="return confirm('Are you sure, you want to delete this Comment?')" method="post" style="display: inline;" v-if="isShowCommentBox != {{$comment->id}}">
-                                                        @csrf
-                                                        @method('delete')
-                                                        <button type="submit" class="bg-transparent border-0 small text-secondary">Delete</button>
-                                                    </form>
-                                                @endif
-                                            @endif  
-                                        </div>
-                                
-                                    </div>
-                                </li>
-                            @endforeach
-                        </ul>
+                                        {{--<div class="col-md-6 text-right">--}}
+                                            {{--@if(Auth::user())--}}
+                                                {{--@if(Auth::user()->id == $comment->user_id) --}}
+                                                    {{--<button class="bg-transparent border-0 small text-secondary" @click="isComment({{$comment->id}})"  v-if="isShowCommentBox != {{$comment->id}}">Edit</button>--}}
+                                                    {{--<button class="bg-transparent border-0 small text-secondary" @click="isComment(null)"  v-if="isShowCommentBox == {{$comment->id}}">Cancel</button>--}}
+                                                    {{--<form action="{{ route('comment.destroy', $comment->id)}}" onclick="return confirm('Are you sure, you want to delete this Comment?')" method="post" style="display: inline;" v-if="isShowCommentBox != {{$comment->id}}">--}}
+                                                        {{--@csrf--}}
+                                                        {{--@method('delete')--}}
+                                                        {{--<button type="submit" class="bg-transparent border-0 small text-secondary">Delete</button>--}}
+                                                    {{--</form>--}}
+                                                {{--@endif--}}
+                                            {{--@endif  --}}
+                                        {{--</div>--}}
+                                {{----}}
+                                    {{--</div>--}}
+                                {{--</li>--}}
+                            {{--@endforeach--}}
+                        {{--</ul>--}}
                     </div>     
                 </div>
             </div>
