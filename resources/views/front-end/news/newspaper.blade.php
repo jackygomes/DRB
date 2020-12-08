674faa177004b5035389dc95da4b2d3f1f4ce8b5
Mahfuzur | WebAble <mahfuzur.rahman@webable.com.bd>
2020-12-08 17:28:16 +0600

    removed add from newspaper page

--- resources/views/front-end/news/newspaper.blade.php
+++ resources/views/front-end/news/newspaper.blade.php
@@ -3,30 +3,30 @@
 
     @include('front-end.news.newspaper-slide')
 
-    <section>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-8 offset-2">
-                    <div class="financial_top_add mb-3">
+    {{--<section>--}}
+        {{--<div class="container-fluid">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-8 offset-2">--}}
+                    {{--<div class="financial_top_add mb-3">--}}
                         {{--add section--}}
-                        <div class="financial_top_add mb-3">
-                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                            <!-- Horizontal unit -->
-                            <ins class="adsbygoogle"
-                                 style="display:block"
-                                 data-ad-client="ca-pub-7889950300474908"
-                                 data-ad-slot="9066843834"
-                                 data-ad-format="auto"
-                                 data-full-width-responsive="true"></ins>
-                            <script>
-                                (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+                        {{--<div class="financial_top_add mb-3">--}}
+                            {{--<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>--}}
+                            {{--<!-- Horizontal unit -->--}}
+                            {{--<ins class="adsbygoogle"--}}
+                                 {{--style="display:block"--}}
+                                 {{--data-ad-client="ca-pub-7889950300474908"--}}
+                                 {{--data-ad-slot="9066843834"--}}
+                                 {{--data-ad-format="auto"--}}
+                                 {{--data-full-width-responsive="true"></ins>--}}
+                            {{--<script>--}}
+                                {{--(adsbygoogle = window.adsbygoogle || []).push({});--}}
+                            {{--</script>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</section>--}}
 
     <section class="news" id="app">
         <div class="container-fluid">
@@ -41,8 +41,9 @@
                 <div class="col-12 col-md-2">
                     <div class="wrapper">
                         <!-- Sidebar  -->
+                        {{--news-sidenav-scroll-hide--}}
                         <nav id="sidebar"
-                             class="bg-transparent text-dark custom-news-nav-header-top news-sidenav-scroll-hide" style="width: 235px; margin-top: 150px;">
+                             class="bg-transparent text-dark custom-news-nav-header-top " style="width: 235px; margin-top: 150px;">
 
                             <ul class="list-unstyled components">
                                 <li class="{{ request()->url() == route('news.index') ? 'news-sidenav-active' : '' }}">
